f9869a96e6e09eeafe5d3c4d27b13c9bf1c733b2
Victoria Belova <vacova1988@gmail.com>
2022-02-23 20:33:58 +0300

    init: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let arr = str.split("");
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    let a = bracketsConfig[i][0];
+    let b = bracketsConfig[i][1];
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] == a && arr[j + 1] == b) {
+        arr.splice(j, 2);
+        i = -1;
+      }
+    }
+  }
+  return !arr.length;
+};
